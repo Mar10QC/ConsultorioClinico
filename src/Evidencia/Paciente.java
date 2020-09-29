5d970a70bddb37c8729649959293357647494cc8
Mario <marquicor@gmail.com>
2020-09-28 21:57:21 -0500

    creaciónClases

---
+++ src/Evidencia/Paciente.java
@@ -0,0 +1,11 @@
+package Evidencia;
+
+public class Paciente {
+    int id_paciente;
+    String nombre;
+
+    public Paciente(int id_paciente, String nombre){
+        this.id_paciente = id_paciente;
+        this.nombre = nombre;
+    }
+}
